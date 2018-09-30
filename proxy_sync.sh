30c65ce6beec2d85b844e5c7862d07f2e6e580a4
chi <csun@proclivitysystems.com>
2018-10-01 00:20:50 +0800

    add proxy sync script

---
+++ proxy_sync.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+files=($(git diff-tree --no-commit-id --name-only -r HEAD | tr '\n' ' '))
+
+echo Found changed files:
+
+len=${#files[@]}
+for((i=0;i<$len;i++))
+do
+    echo "  ${files[i]}"
+    files[i]=${files[i]%%/*}
+done
+
+for((i=0;i<$len;i++))
+do
+    for((j=$len-1;j>i;j--))
+    do
+        if [[ ${files[i]} = ${files[j]} ]]; then
+        unset array[i]
+        fi
+    done
+done
+
+pwd=$(pwd)
+
+cd /code/apigee_automation/
+source ./setenv.sh
+
+for proxy in $files;
+do
+    path="$pwd/$proxy"
+
+    if [ -f "$path/$proxy.json" ]; then
+        echo Create open API proxies
+        openapi2apigee generateApi $proxy --source $path/apiproxy/$proxy.json --deploy --destination /tmp/ --baseuri $APIGEE_URL --organization $APIGEE_ORG --environments $APIGEE_ENV --virtualhosts default --username $APIGEE_USER --password $APIGEE_PASSWORD
+        if [ -d "$path/apiproxy" ]; then
+            flows=$(grep -ozP '(?s)<Flows>(?:(?!Flows).).*(?:(?!Flows).)*?</Flows>' /tmp/$proxy/apiproxy/proxies/default.xml)
+            flows=$(echo ${flows} | tr -d '\n' | sed -e "s|&quot;|\"|g")
+            target_url=$(grep -ozP '(?s)<URL>(.*)</URL>' /tmp/$proxy/apiproxy/targets/default.xml)
+
+            rm -fr /tmp/$proxy
+            cp -r $path /tmp
+
+            sed -i -e "s|<Flows\/>|$flows|g" /tmp/$proxy/apiproxy/proxies/default.xml
+            sed -i -e "s|<URL\/>|$target_url|g" /tmp/$proxy/apiproxy/targets/default.xml
+
+            echo "Deploy /tmp/$proxy/"
+            source ./proxy_deploy.sh $proxy /tmp/$proxy/
+        fi
+    else
+        if [ -d "$path" ]; then
+            statusCode="$(curl -Is $APIGEE_URL/v1/organizations/$APIGEE_ORG/apis/$proxy -u $APIGEE_USER:$APIGEE_PASSWORD | head -n 1)"
+
+            if [[ $statusCode = *"HTTP/1.1 404"* ]]; then
+                echo "Create proxy $proxy"
+                curl -H "Content-type:application/json" -X POST -d "{\"name\" : \"$proxy\"}"  $APIGEE_URL/v1/organizations/$APIGEE_ORG/apis/ -u $APIGEE_USER:$APIGEE_PASSWORD
+            fi
+
+            echo "Deploy $path"
+            source ./proxy_deploy.sh $proxy $path
+        fi
+    fi
+done;
+
--- setenv.sh
+++ setenv.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
 ## Update below section for your environment.
-## Org is the name of the organization in which you have an account. 
+## Org is the name of the organization in which you have an account.
 ## Login to enterprise.apigee.com and check account settings.
-## Credentials are the username and password that you use to login 
+## Credentials are the username and password that you use to login
 ## to enterprise.apigee.com
-## Trial and cloud accounts have environments called 'test' and 'prod'. 
+## Trial and cloud accounts have environments called 'test' and 'prod'.
 ## If you have a trial or cloud account, you do not need to
 ## modify the URL.
 ## You can obtain a free account at https://accounts.apigee.com/accounts/sign_up
 ## On-premise deployments may have other environments.
 ## --------------------------------------
 ## Cloud users: Leave the default values for $url and $api_domain.
-## On-prem customers: 
+## On-prem customers:
 ## - Change the $url to your Apigee management server.
 ## - Change $api_domain to the base domain for your own Apigee API calls.
 ##   $api_domain gets used in conjunction with $org and $env to construct
 ##   the base URL to your APIs: https://$org-$env.$api_domain/<api_resource>
+export APIGEE_USER=chi@darumatic.com
+export APIGEE_PASSWORD=apigee123@A
+
+
+#this usually doesn't change
+export APIGEE_ORG=chi-eval
+export APIGEE_URL=https://api.enterprise.apigee.com
+export APIGEE_API_DOMAIN=apigee.net
+
+#this is specific to the proxy being created
+export APIGEE_ENV=test
 
 org="${APIGEE_ORG}"
 username="${APIGEE_USER}"
@@ -31,3 +42,5 @@ export username=$username
 export env=$env
 export url=$url
 export api_domain=$api_domain
+
+
