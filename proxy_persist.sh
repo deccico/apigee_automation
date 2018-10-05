eeaf5314852f1cd88074756e33d0d92987a43492
Adrián Deccico <adrian@darumatic.com>
2018-10-05 11:38:34 +1000

    fixing git persistence from local execution

--- proxy_persist.sh
+++ proxy_persist.sh
@@ -1,14 +1,23 @@
 #!/bin/bash -ex
+set -o nounset
+
+echo ------------------------
+echo Persisting proxy in Git
+
+pwd
+
+mkdir -p /tmp/.ssh
+SSH_FILE=/tmp/.ssh/id_rsa
 
 if [ ! -z ${SSH_PRIVATE_KEY+x} ]; then
     echo Push $proxy proxy config
-    echo "${SSH_PRIVATE_KEY}" > /tmp/.id_rsa
-    cd $pwd
-    git config --global user.name "Jenkins Agent"
-    git config --global user.email "Jenkins_Agent@localhost"
-    git add $proxy
-    git commit -m "adding $proxy proxy config"
-    GIT_SSH_COMMAND="ssh -o 'StrictHostKeyChecking no' -i /tmp/.id_rsa" git push origin $git_branch
-    cd /code/apigee_automation/
+    echo "${SSH_PRIVATE_KEY}" > $SSH_FILE
+    chmod 400 $SSH_FILE
 fi
+
+git config --global user.name "Jenkins Agent"
+git config --global user.email "Jenkins_Agent@localhost"
+git add $proxy
+git commit -m "adding $proxy proxy config"
+GIT_SSH_COMMAND="ssh -o 'StrictHostKeyChecking no' -i $SSH_FILE" git push origin $git_branch
 
