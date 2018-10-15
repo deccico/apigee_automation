209612dc7d5b2df563bcb468c6a7733d73f86906
Chi <>
2018-10-15 21:44:33 +0800

    fix git persist script

--- proxy_persist.sh
+++ proxy_persist.sh
@@ -4,21 +4,13 @@ set -o nounset
 echo ------------------------
 echo Persisting proxy in Git
 
-pwd
-
-mkdir -p /tmp/.ssh
-SSH_FILE=/tmp/.ssh/id_rsa
-
-if [ ! -z ${SSH_PRIVATE_KEY+x} ]; then
-    echo Push $proxy proxy config
-    echo "${SSH_PRIVATE_KEY}" > $SSH_FILE
-    chmod 400 $SSH_FILE
-fi
-
 git config --global user.name "Jenkins Agent"
 git config --global user.email "Jenkins_Agent@localhost"
 git add api-proxies/$proxy
 git commit -m "adding $proxy proxy config"
+
+url_host=`git remote get-url origin | sed -e "s/https:\/\/gitlab-ci-token:.*@//g"`
+git remote set-url origin "https://gitlab-ci-token:${SSH_PRIVATE_KEY}@${url_host}"
 echo git push origin $(git rev-parse HEAD):$git_branch
-GIT_SSH_COMMAND="ssh -o 'StrictHostKeyChecking no' -i $SSH_FILE" git push origin $(git rev-parse HEAD):$git_branch
+git push origin $(git rev-parse HEAD):$git_branch
 
